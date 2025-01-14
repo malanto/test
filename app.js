b89e37a499500e71a93a04e3d3ca5aab30a9f159
malanto <78157677+malanto@users.noreply.github.com>
2025-01-14 05:58:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ const server = http.createServer((req, res) => {
     if (req.url === '/start' && req.method === 'GET') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Server starting...');
-        runWeb();
+        setTimeout(() => {
+          runWeb();
+        }, 2000);
     } else {
         res.writeHead(405, { 'Content-Type': 'text/html' });
         res.end(`<h1>Method Not Allowed</h1>
