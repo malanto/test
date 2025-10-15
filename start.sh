1d5059e027465c140125a8410412804d80ee26ad
malanto <78157677+malanto@users.noreply.github.com>
2025-10-15 14:56:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 npm start &
 sleep 2
 # 后台运行 tunnel 服务
-./server tunnel --region us --edge-ip-version auto --no-autoupdate --protocol http2 run --token "eyJhIjoiZjcwNjZjZjc4YTgxMTJiNGZiNWI4OTE1M2VjMGE0YWIiLCJ0IjoiOTA5ZDBjZTctNGVkZS00MDk1LTg0YTMtOTQxNjNkZWE1ZTM1IiwicyI6Ik9USXdaR1JtTjJVdE5HSTROaTAwTW1RMkxXRXlabUV0WkRJeE9UbGpNakJoT1RSbCJ9"
+./server tunnel --region us --edge-ip-version auto --no-autoupdate --protocol http2 run --token $token
